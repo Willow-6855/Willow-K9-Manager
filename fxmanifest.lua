4571a7f449f87b10f63cc1b50e3dd3fe58c4cca0
Willow-6855 <104696802+Willow-6855@users.noreply.github.com>
2023-10-07 15:25:23 -0400

    Initial version
    Initial version

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+game 'gta5'
+
+name "Willow K9 Manager"
+description "Menu to spawn and manage your K9"
+author "Willow Development"
+"
+version "1.0.0"
+
+client_scripts {
+    'RageUI/RMenu.lua',
+    'RageUI/menu/RageUI.lua',
+    'RageUI/menu/Menu.lua',
+    'RageUI/menu/MenuController.lua',
+    'RageUI/components/*.lua',
+    'RageUI/menu/elements/*.lua',
+    'RageUI/menu/items/*.lua',
+    'RageUI/menu/panels/*.lua',
+    'RageUI/menu/windows/*.lua',
+    'client.lua',
+}
